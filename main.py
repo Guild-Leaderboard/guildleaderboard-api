32c7b1035b5ccfd2b83dc28b3632d24fcc351ef9
timnoot <82733075+timnoot@users.noreply.github.com>
2022-09-13 21:12:20 +0200

    changed to fastapi

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import asyncio
 import datetime
+import logging
 import os
 import re
+from logging.config import dictConfig
 from math import sin, ceil
 
 import aiohttp
 from dotenv import load_dotenv
-from quart import Quart, jsonify, request
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from custom_logger import LogConfig
 from utils.database import Database
 
 load_dotenv(".env")
+dictConfig(LogConfig().dict())
 
 
 def weight_multiplier(members):
@@ -198,13 +203,14 @@ class DatabaseCache:
                     del self.guild_metrics[key]
 
 
-class App(Quart):
+class App(FastAPI):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.db: Database = None
         self.loop: asyncio.BaseEventLoop = None
         self.session: aiohttp.ClientSession = None
         self.database_cache: DatabaseCache = DatabaseCache(self)
+        self.logger = logging.getLogger("mycoolapp")
         self.patrons = None
         self.patreon_last_get = 0
 
@@ -214,16 +220,27 @@ class App(Quart):
             return data["message"].replace(" patrons", "")
 
 
-app: App = App(__name__)
+app: App = App()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://guildleaderboard.com",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.route("/leaderboard")
+@app.get("/leaderboard")
 async def leaderboard():
     r = await app.database_cache.get_guilds()
-    return jsonify(r)
+    return r
 
 
-@app.route("/stats")
+@app.get("/stats")
 async def stats():
     r = await app.database_cache.get_guilds()
     guilds_tracked = len(r)
@@ -232,74 +249,40 @@ async def stats():
         app.patrons = await app.get_patreon_members()
         app.patreon_last_get = Time().time
 
-    return jsonify({
+    return {
         "guilds_tracked": guilds_tracked,
         "players_tracked": players_tracked,
         "patrons": app.patrons,
-    })
+    }
 
 
-@app.route("/guild/<guild_id>")
-async def guild(guild_id):
+@app.get("/guild/{guild_id}")
+async def guild(guild_id: str):
     r = await app.database_cache.get_guild(guild_id)
     try:
         r2 = r.copy()
         del r2["get_time"]
     except:
         r2 = r
-    return jsonify(r2)
+    return r2
 
 
-@app.route("/metrics/<guild_id>")
-async def metrics(guild_id):
+@app.get("/metrics/{guild_id}")
+async def metrics(guild_id: str):
     app.logger.error('Deprecated endpoint used: /metrics/<guild_id>')
     return await guild_metrics(guild_id)
 
 
-@app.route("/metrics/guild/<guild_id>")
-async def guild_metrics(guild_id):
+@app.get("/metrics/guild/{guild_id}")
+async def guild_metrics(guild_id: str):
     r = await app.database_cache.get_guild_metrics(guild_id)
-    if not r:
-        return jsonify([])
-
-    if request.args.get('format', False):
-        guild_name = (await app.db.pool.fetchrow("""
-SELECT guild_name AS name FROM guilds WHERE guild_id = $1 LIMIT 1;
-        """, guild_id))["name"]
-
-        r3 = {
-            "catacombs_data": [],
-            "skills_data": [],
-            "slayer_data": [],
-            "senither_weight_data": [],
-            "lily_weight_data": [],
-            "member_count_data": []
-        }
-
-        for guild_metric in r:
-            r3["catacombs_data"].append([guild_metric['capture_date'], guild_metric["catacombs"]])
-            r3["skills_data"].append([guild_metric['capture_date'], guild_metric["skills"]])
-            r3["slayer_data"].append([guild_metric['capture_date'], guild_metric["slayer"]])
-            r3["senither_weight_data"].append([guild_metric['capture_date'], guild_metric["senither_weight"]])
-            r3["member_count_data"].append([guild_metric['capture_date'], guild_metric["member_count"]])
-            if guild_metric.get("lily_weight"):
-                r3["lily_weight_data"].append([guild_metric['capture_date'], guild_metric["lily_weight"]])
-
-        r4 = {}
-        for key, value in r3.items():
-            r4[key] = {
-                "name": guild_name,
-                "data": value
-            }
-        return jsonify(r4)
-
-    return jsonify(r)
+    return r if r else []
 
 
-@app.route("/metrics/player/<uuid>")
+@app.get("/metrics/player/{uuid}")
 async def player_metrics(uuid):
     r = await app.database_cache.get_player_metrics(uuid)
-    return jsonify(r)
+    return r
 
 
 def fix_history_order(history: list) -> list:
@@ -334,54 +317,40 @@ def fix_history_order(history: list) -> list:
     return list(reversed(new_history))
 
 
-@app.route("/v2/history")
-async def history_v2():
-    guild_id = request.args.get("guild_id", None)
-    player = request.args.get("uuid", None)
-    per_page = int(request.args.get("per_page", 10))
-    page = int(request.args.get("page", 1))
+@app.get("/v2/history")
+async def history_v2(guild_id: str = None, uuid: str = None, per_page: int = 10, page: int = 1):
     if page < 1:
         page = 1
     if per_page < 1:
         per_page = 10
-    if not player and not guild_id:
-        return jsonify(None)
+    if not uuid and not guild_id:
+        return None
 
-    r, total_rows = await app.db.get_history_v2(guild_id, player, per_page, page, return_total=True)
+    r, total_rows = await app.db.get_history_v2(guild_id, uuid, per_page, page, return_total=True)
 
-    return jsonify({
-        "data": fix_history_order(r) if player else r,
+    return {
+        "data": fix_history_order(r) if uuid else r,
         "paginate": {
             "current_page": page,
             "last_page": ceil(total_rows / per_page),
             "total": total_rows
         }
-    })
+    }
 
 
-@app.route("/player/<uuidorname>")
-async def player(uuidorname):
+@app.get("/player/{uuidorname}")
+async def player(uuidorname: str):
     r = await app.database_cache.get_player(uuidorname)
-    return jsonify(r)
+    return r
 
 
-@app.route("/autocomplete")
+@app.get("/autocomplete")
 async def autocomplete():
     r = await app.db.get_id_name_autocomplete()
-    return jsonify(r)
+    return r
 
 
-@app.errorhandler(404)
-async def page_not_found(e):
-    return jsonify(404), 404
-
-
-@app.errorhandler(500)
-async def internal_error(e):
-    return jsonify(500), 500
-
-
-@app.before_serving
+@app.on_event('startup')
 async def start_up():
     if not app.loop:
         app.loop = asyncio.get_event_loop()
@@ -394,13 +363,10 @@ async def start_up():
         app.patreon_last_get = Time().time
 
 
-@app.after_request
-def set_headers(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+if __name__ == "__main__":
+    import uvicorn
 
-
-if os.name == "nt":
-    app.run(host="0.0.0.0", port=8080, use_reloader=False, debug=True)
-else:
-    app.run(host="0.0.0.0", port=80, debug=True, use_reloader=False)
+    if os.name == 'nt':
+        uvicorn.run("main:app", reload=True, port=8080, host="0.0.0.0")
+    else:
+        uvicorn.run("main:app", reload=True, port=80, host="0.0.0.0")
